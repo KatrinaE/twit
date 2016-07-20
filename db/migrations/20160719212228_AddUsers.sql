67e04a5f9e6fa907face5163e5efac0844807b43
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-07-19 21:34:02 -0400

    Add AddUsers migration

---
+++ db/migrations/20160719212228_AddUsers.sql
@@ -0,0 +1,12 @@
+
+-- +goose Up
+INSERT INTO t_user (id, username) VALUES (1, 'ben');
+INSERT INTO t_user (id, username) VALUES (2, 'alyssa');
+INSERT INTO t_user (id, username) VALUES (3, 'louis');
+INSERT INTO t_user (id, username) VALUES (4, 'eva');
+INSERT INTO t_user (id, username) VALUES (5, 'cy');
+
+
+-- +goose Down
+DELETE FROM t_user WHERE id IN('1', '2', '3', '4');
+
