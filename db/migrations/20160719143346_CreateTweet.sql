1a38ba8fad44ec212f9a1b6f71b651ecd7b66cc2
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-07-19 22:12:37 -0400

    Switch id columns from varchar to serial

--- db/migrations/20160719143346_CreateTweet.sql
+++ db/migrations/20160719143346_CreateTweet.sql
@@ -1,8 +1,8 @@
 
 -- +goose Up
 CREATE TABLE t_tweet (
-id		VARCHAR(16) PRIMARY KEY,
-user_id		VARCHAR(16) REFERENCES t_user(id),
+id		SERIAL PRIMARY KEY,
+user_id		INTEGER REFERENCES t_user(id),
 message		VARCHAR(160),
 ctime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
 mtime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
--- db/migrations/20160719143827_CreateFollower.sql
+++ db/migrations/20160719143827_CreateFollower.sql
@@ -1,9 +1,9 @@
 
 -- +goose Up
 CREATE TABLE t_follower (
-id		VARCHAR(16) PRIMARY KEY,
-follower_id	VARCHAR(16) REFERENCES t_user(id),
-followed_id	VARCHAR(16) REFERENCES t_user(id),
+id		SERIAL PRIMARY KEY,
+follower_id	INTEGER REFERENCES t_user(id),
+followed_id	INTEGER REFERENCES t_user(id),
 ctime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
 mtime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
