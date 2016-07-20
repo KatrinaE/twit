fe83956424903d1ccf6b1857c2321ca0168dfd2e
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-07-19 21:33:07 -0400

    Update CreateUserTimeline

--- db/migrations/20160719144447_CreateUserTimeline.sql
+++ db/migrations/20160719144447_CreateUserTimeline.sql
@@ -6,11 +6,10 @@ user_id		VARCHAR(16) REFERENCES t_user(id),
 tweets		VARCHAR(160),
 ctime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
 mtime		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
-
-)
+);
 
 
 
 -- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE t_user_timeline;
 
