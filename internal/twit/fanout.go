5bf2a64281df951f20166b425811b8b8aa196aa3
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-07-24 09:20:39 -0400

    Add logging statements to fanoutworker

--- internal/twit/fanout.go
+++ internal/twit/fanout.go
@@ -2,6 +2,7 @@ package twit
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"time"
 )
@@ -9,11 +10,23 @@ import (
 func fanout(tweet Tweet) {
 	followerA, err := dbQryUserFollowers(tweet.UserId)
 	if err != nil {
-		log.Print("Could not insert tweet %+v. Err: %+v", tweet, err)
+		m := "Could not get followers for tweet "
+		m += fmt.Sprintf("%+v. Err: %+v", tweet, err)
+		log.Print(m)
 		return
 	}
 	for _, follower := range followerA {
-		redisInsertTweet(follower.Id, tweet)
+		err := redisInsertTweet(follower.Id, tweet)
+		if err != nil {
+			m := "Could not insert tweet "
+			m += fmt.Sprintf("%+v into %d's ", tweet, follower.Id)
+			m += fmt.Sprintf("home timeline. Err: %+v", err)
+			log.Print(m)
+			return
+		}
+		m := fmt.Sprintf("Inserted tweet %+v into ", tweet)
+		m += fmt.Sprintf("user %d's home timeline", follower.Id)
+		log.Print(m)
 	}
 }
 
@@ -22,18 +35,35 @@ func FanoutLoop() {
 		tweetId, err := dbGetNextQueuedTweetId()
 		switch {
 		case err == sql.ErrNoRows:
-			time.Sleep(300 * time.Millisecond)
+			time.Sleep(3000 * time.Millisecond)
+			continue
 		case err != nil:
-			log.Print("%+v", err)
+			log.Printf("%+v", err)
 			// do not terminate execution -- maybe
 			// revisit this decision later
+			continue
 		}
-		dbMarkTweetProcessing(tweetId)
 		tweet, err := dbGetTweet(tweetId)
 		if err != nil {
-			// todo: what about situations where you can't
-			// connect to the database at all?
+			m := fmt.Sprintf("Could not get tweet %d, ", tweetId)
+			m += "even though it was queued. "
+			m += fmt.Sprintf("Err: %+v", err)
+			log.Print(m)
 			dbMarkTweetErrored(tweetId)
+			if err != nil {
+				m := "Could not mark tweet "
+				m += fmt.Sprintf("%d", tweetId)
+				m += " errored. Error: "
+				m += fmt.Sprintf("%+v", err)
+				log.Print(m)
+			}
+			continue
+		}
+		err = dbMarkTweetProcessing(tweetId)
+		if err != nil {
+			m := fmt.Sprintf("Could not mark tweet %d ", tweetId)
+			m += "processing. Aborting fanout."
+			log.Print(m)
 			continue
 		}
 		fanout(tweet)
