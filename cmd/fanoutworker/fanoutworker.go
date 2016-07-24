03a722a4870a873c1a25e29598568cefa84bf2e6
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-07-24 08:59:58 -0400

    Add flags + logging statements to fanout worker

--- cmd/fanoutworker/fanoutworker.go
+++ cmd/fanoutworker/fanoutworker.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/spf13/viper"
 	"local/twit/internal/twit"
+	"log"
 )
 
 func main() {
+	log.Print("Starting fanout worker...")
+	log.Print("Parsing flags")
+	var configPath string
+	var configFilename string
+	flag.StringVar(&configPath, "configpath", ".",
+		"path to configuration file (absolute or relative)")
+	flag.StringVar(&configFilename, "configfile", "config",
+		"name of config file (no extension")
+	flag.Parse()
+
+	log.Print("Setting config")
 	viper.SetConfigType("yaml")
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
+	viper.SetConfigName(configFilename)
+	viper.AddConfigPath(configPath)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	fmt.Println("a fanout loop")
+
+	log.Print("Running fanout loop")
 	twit.FanoutLoop()
-	fmt.Println("fanout loop done")
 }
