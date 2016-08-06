959f9abfa0eec430d1c9febad53331cac879392d
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2016-08-05 21:37:07 -0400

    Move db/testdata.txt to db/createTestRecords.sql

--- db/createTestRecords.sql
+++ db/createTestRecords.sql
@@ -10,10 +10,11 @@ INSERT INTO t_follower (id, follower_id, followed_id) VALUES (3, 4, 1);
 
 INSERT INTO t_tweet (id, user_id, message) VALUES (1, 1, 'i have lots of followers');
 INSERT INTO t_tweet (id, user_id, message) VALUES (2, 1, 'my second tweet');
-INSERT INTO t_tweet (id, user_id, message) VALUES (2, 2, 'i have no followers');
+INSERT INTO t_tweet (id, user_id, message) VALUES (3, 2, 'i have no followers');
 
-INSERT INTO t_tweet_queue (tweet_id) VALUES (1);
-INSERT INTO t_tweet_queue (tweet_id) VALUES (2);
+INSERT INTO t_tweet_queue (tweet_id, status) VALUES (1, 'ready');
+INSERT INTO t_tweet_queue (tweet_id, status) VALUES (2, 'ready');
+INSERT INTO t_tweet_queue (tweet_id, status) VALUES (3, 'ready');
 
 -- DELETE FROM t_tweet_queue;
 -- DELETE FROM t_tweet;
